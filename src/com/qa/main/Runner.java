c0774c06eb2b3a5dc729ddb77034a7560db19bb0
Korbinian <kmr.ring@outlook.com>
2020-03-26 17:13:42 +0000

    added the one that really matters..

--- src/com/qa/main/Runner.java
+++ src/com/qa/main/Runner.java
@@ -3,31 +3,30 @@ package com.qa.main;
 public class Runner {
 
 	public static void main(String[] args) {
-		Q1_Numbers Numbers = new Q1_Numbers();
-		Numbers.crossSum(42);
-		Numbers.crossSum(99);
-		Numbers.crossSum(1);
-		
-		System.out.println(Numbers.nameThatNumber(55));
-		System.out.println(Numbers.nameThatNumber(19));
-		System.out.println(Numbers.nameThatNumber(99));
-		
-		System.out.println("\n"); //now up to 999
-		Q2_NumbersTo999 Numbers2 = new Q2_NumbersTo999();
-		System.out.println(Numbers2.nameThatNumber(111));
-		System.out.println(Numbers2.nameThatNumber(17));
-		System.out.println(Numbers2.nameThatNumber(858));
-		
-		System.out.println("\n"); //now up to 9999, some examples:
-		Q3_NumersTo9999 Numbers3 = new Q3_NumersTo9999();
-		System.out.println(Numbers3.nameThatNumber(1000));
-		System.out.println(Numbers3.nameThatNumber(7016));
-		System.out.println(Numbers3.nameThatNumber(17));
-		System.out.println(Numbers3.nameThatNumber(8585));
-		System.out.println(Numbers3.nameThatNumber(7770));
-		System.out.println(Numbers3.nameThatNumber(7700));
-		System.out.println(Numbers3.nameThatNumber(10000));
-		
+//		Q1_Numbers Numbers = new Q1_Numbers();
+//		Numbers.crossSum(42);
+//		Numbers.crossSum(99);
+//		Numbers.crossSum(1);
+//
+//		System.out.println(Numbers.nameThatNumber(19));
+//		System.out.println(Numbers.nameThatNumber(99));
+//
+//		System.out.println("\n"); // now up to 999
+//		Q2_NumbersTo999 Numbers2 = new Q2_NumbersTo999();
+//		System.out.println(Numbers2.nameThatNumber(111));
+//		System.out.println(Numbers2.nameThatNumber(858));
+//
+//		System.out.println("\n"); // now up to 9999, some examples:
+//		Q3_NumersTo9999 Numbers3 = new Q3_NumersTo9999();
+//		System.out.println(Numbers3.nameThatNumber(1000));
+//		System.out.println(Numbers3.nameThatNumber(5678));
+
+		ExtraNumbersTo2147483647 extraNumbers = new ExtraNumbersTo2147483647();
+		long Num = 2147425254342583643L;
+		int length = (extraNumbers.howLong(Num));
+		int[] Split = extraNumbers.splitIntoArray(Num, length);
+		extraNumbers.nameThatNumber(Split, length);
+
 	}
 
 }
