20f55d6ae8b87cdbeed2f196784ac6fe8faf090a
Korbinian <kmr.ring@outlook.com>
2020-03-29 12:50:44 +0100

    improved version of BetterNumberNamer can name any positive and negative integer with magnitude less than 10^102.

--- src/com/qa/main/Runner.java
+++ src/com/qa/main/Runner.java
@@ -4,11 +4,11 @@ public class Runner {
 
 	public static void main(String[] args) {
 
-		Q1_Numbers Numbers = new Q1_Numbers();
-		Numbers.crossSum(42);
-		Numbers.crossSum(99);
-		Numbers.crossSum(1);
-
+//		Q1_Numbers Numbers = new Q1_Numbers();
+//		Numbers.crossSum(42);
+//		Numbers.crossSum(99);
+//		Numbers.crossSum(1);
+//
 //		System.out.println(Numbers.nameThatNumber(19));
 //		System.out.println(Numbers.nameThatNumber(99));
 //
@@ -29,13 +29,13 @@ public class Runner {
 //		extraNumbers.nameThatNumber(Split, length);
 
 		System.out.println(
-				"\nup to 101 digits: There is no english name given to most numbers above the magnitude 99E101.\n");
+				"up to 102 digits: There is no english name given to most numbers above the magnitude 99E101.\n");
 
-		ChunkCounter chunkCounter = new ChunkCounter();
-		chunkCounter.chunker(
+		BetterNumberNamer betterNumberNamer = new BetterNumberNamer();
+		betterNumberNamer.chunker(
 				"-11022552345432023332342300024243234252300452345234524333312341341234123461536361435242387422340002441");
-		System.out.println("");
-		chunkCounter.chunker("0");
+		System.out.println("\n");
+		betterNumberNamer.chunker("0");
 		System.out.println("");
 	}
 
